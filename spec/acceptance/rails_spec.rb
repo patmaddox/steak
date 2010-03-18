525fd6d74ab91768dc8bded57f2e3070ff06cae1
Pat Maddox <pat.maddox@gmail.com>
2010-03-18 11:57:17 -0700

    No need to include Have or Be matchers, RSpec does it already

--- spec/acceptance/rails_spec.rb
+++ spec/acceptance/rails_spec.rb
@@ -38,4 +38,27 @@ feature "Acceptance spec execution", %q{
     output.should =~ /1 example, 0 failures/
   end
   
+  scenario "Be matcher shouldn't break" do
+    rails_app = create_rails_app
+    spec_file = create_spec :path    => rails_app + "/spec/acceptance",
+                            :content => <<-SPEC
+      require File.dirname(__FILE__) + "/acceptance_helper.rb"
+      feature "Minimal spec" do
+        scenario "First scenario" do
+          object = Object.new
+          def object.super_duper?
+            true
+          end
+          def object.lame?
+            false
+          end
+          object.should be_super_duper
+          object.should_not be_lame
+        end
+      end
+    SPEC
+    output = run_spec spec_file
+    output.should =~ /1 example, 0 failures/
+  end
+
 end
